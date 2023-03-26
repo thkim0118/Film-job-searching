8274728ae6215341697e13823e25e5f4b45f062c
TaehyungKim <th-kim>
2023-03-26 23:40:27 +0900

    add : 디자인 style guide, token 적용

--- app/src/main/java/com/fone/filmone/ui/theme/Theme.kt
+++ app/src/main/java/com/fone/filmone/ui/theme/Theme.kt
@@ -7,24 +7,9 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -37,7 +22,6 @@ fun FilmOneTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
         shapes = Shapes,
         content = content
     )
