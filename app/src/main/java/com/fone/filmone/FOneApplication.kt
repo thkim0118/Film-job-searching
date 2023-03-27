96ba5e76a8d34bbfa446097f5cc9b163c8f62453
TaehyungKim <th-kim>
2023-03-27 22:36:51 +0900

    refactor : 패키지 변경 및 Application 클래스 생성

---
+++ app/src/main/java/com/fone/filmone/FOneApplication.kt
@@ -0,0 +1,8 @@
+package com.fone.filmone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FOneApplication : Application() {
+}
--- app/src/main/java/com/fone/filmone/ui/MainActivity.kt
+++ app/src/main/java/com/fone/filmone/ui/MainActivity.kt
@@ -1,15 +1,8 @@
-package com.fone.filmone
+package com.fone.filmone.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.fone.filmone.ui.theme.FilmOneTheme
 
 class MainActivity : ComponentActivity() {
