68998ede7ed3a26ea56ac342c98b3b893143a8af
TaehyungKim <th-kim>
2023-03-29 00:16:20 +0900

    add : 공통 TextStyle 추가

---
+++ app/src/main/java/com/fone/filmone/ui/common/ext/FTextStyle.kt
@@ -0,0 +1,24 @@
+package com.fone.filmone.ui.common.ext
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import com.fone.filmone.ui.theme.Pretendard
+
+fun fTextStyle(
+    fontWeight: FontWeight,
+    fontSize: TextUnit,
+    lineHeight: TextUnit,
+    color: Color,
+    fontFamily: FontFamily = Pretendard,
+): TextStyle {
+    return TextStyle(
+        fontWeight = fontWeight,
+        fontSize = fontSize,
+        lineHeight = lineHeight,
+        color = color,
+        fontFamily = fontFamily,
+    )
+}
