6495adfe634cac37fdc32b8f8559772ec0f8106f
TaehyungKim <choir0118@gmail.com>
2023-04-17 18:07:42 +0900

    fix : 사용하지 않는 코드 삭제

--- app/src/main/java/com/fone/filmone/ui/main/model/BottomNavItem.kt
+++ app/src/main/java/com/fone/filmone/ui/main/model/BottomNavItem.kt
@@ -5,38 +5,28 @@ import androidx.annotation.StringRes
 import com.fone.filmone.R
 
 enum class BottomNavItem(
-    val index: Int,
     @DrawableRes val selectedIconRes: Int,
     @DrawableRes val unselectedIconRes: Int,
-    val label: String,
     @StringRes val title: Int,
 ) {
     Home(
-        index = 0,
         selectedIconRes = R.drawable.main_home_selected,
         unselectedIconRes = R.drawable.main_home_unselected,
-        label = "Home",
         title = R.string.main_home_title
     ),
     Job(
-        index = 1,
         selectedIconRes = R.drawable.main_job_selected,
         unselectedIconRes = R.drawable.main_job_unselected,
-        label = "Job",
         title = R.string.main_job_title,
     ),
     Chat(
-        index = 2,
         selectedIconRes = R.drawable.main_chat_selected,
         unselectedIconRes = R.drawable.main_chat_unselected,
-        label = "Chat",
         title = R.string.main_chat_title,
     ),
     My(
-        index = 3,
         selectedIconRes = R.drawable.main_my_selected,
         unselectedIconRes = R.drawable.main_my_unselected,
-        label = "My",
         title = R.string.main_my_title,
     )
 }
