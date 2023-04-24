354bf02ec52c29833b679a5177c77db9a7c1bc55
TaehyungKim <choir0118@gmail.com>
2023-04-24 21:33:02 +0900

    feat : data model enum 값 정의

--- app/src/main/java/com/fone/filmone/data/datamodel/response/jobopenings/Career.kt
+++ app/src/main/java/com/fone/filmone/data/datamodel/response/jobopenings/Career.kt
@@ -1,14 +1,16 @@
 package com.fone.filmone.data.datamodel.response.jobopenings
 
 import androidx.annotation.Keep
+import androidx.annotation.StringRes
+import com.fone.filmone.R
 
 @Keep
-enum class Career {
-    IRRELEVANT,
-    LESS_THAN_10YEARS,
-    LESS_THAN_1YEARS,
-    LESS_THAN_3YEARS,
-    LESS_THAN_6YEARS,
-    MORE_THAN_10YEARS,
-    NEWCOMER
+enum class Career(@StringRes titleRes: Int) {
+    NEWCOMER(R.string.job_opening_career_newcomer),
+    LESS_THAN_1YEARS(R.string.job_opening_career_1year),
+    LESS_THAN_3YEARS(R.string.job_opening_career_3year),
+    LESS_THAN_6YEARS(R.string.job_opening_career_6year),
+    LESS_THAN_10YEARS(R.string.job_opening_career_10year),
+    MORE_THAN_10YEARS(R.string.job_opening_career_10year_more),
+    IRRELEVANT(R.string.job_opening_career_irrelevant),
 }
