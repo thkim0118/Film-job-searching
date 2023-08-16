502a4a87c778aaabf501775a17d8d1de2bc7ed07
Bob Sin <tkagmd1@naver.com>
2023-08-16 23:22:33 +0900

    [FO-238] 글을 쓰다가 흰색의  공백 공간을 누르면 - 키보드가 내려가도록 변경 (#11)
    * [FO-238] 글을 쓰다가 흰색의  공백 공간을 누르면 - 키보드가 내려가도록 변경
    
    * [FO-238] apply code review

--- app/src/main/java/com/fone/filmone/ui/inquiry/InquiryScreen.kt
+++ app/src/main/java/com/fone/filmone/ui/inquiry/InquiryScreen.kt
@@ -1,14 +1,9 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.fone.filmone.ui.inquiry
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -18,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -51,6 +48,7 @@ fun InquiryScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val scrollState = rememberScrollState()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     if (uiState.popScreen) {
         navController.popBackStack()
@@ -81,6 +79,9 @@ fun InquiryScreen(
                     .fillMaxSize()
                     .padding(horizontal = 16.dp)
                     .verticalScroll(scrollState)
+                    .clickableWithNoRipple {
+                        keyboardController?.hide()
+                    }
             ) {
                 Column(
                     modifier = Modifier
