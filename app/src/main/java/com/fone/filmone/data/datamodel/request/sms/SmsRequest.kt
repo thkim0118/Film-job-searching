063f51f44dcaa150464b8152ecd5490789cc5266
Bob Sin <tkagmd1@naver.com>
2023-09-18 02:56:38 +0900

    [FO-453] 이메일 회원가입 SMS 인증번호 오류 (#106)

--- app/src/main/java/com/fone/filmone/data/datamodel/request/sms/SmsRequest.kt
+++ app/src/main/java/com/fone/filmone/data/datamodel/request/sms/SmsRequest.kt
@@ -1,6 +1,10 @@
 package com.fone.filmone.data.datamodel.request.sms
 
+import com.google.gson.annotations.SerializedName
+
 data class SmsRequest(
+    @SerializedName("phone")
     val phone: String,
+    @SerializedName("code")
     val code: String
 )
